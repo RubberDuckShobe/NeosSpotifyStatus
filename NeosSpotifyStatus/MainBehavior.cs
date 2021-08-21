93f9a89f2ee7b991b15f83f705283ea5f4eaedf7
RubberDuckShobe <dog@dog.dog>
2021-08-21 17:43:16 +0200

    Projektdateien hinzufügen.

---
+++ NeosSpotifyStatus/MainBehavior.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using SpotifyAPI.Web;
+
+namespace NeosSpotifyStatus
+{
+    class MainBehavior : WebSocketBehavior
+    {
+        /*
+         * List of the prefixes/commands received from Neos via WebSockets:
+         * 0 - Pause/Resume
+         * 1 - Previous track
+         * 2 - Next track
+         * 3 - Re-request info
+         * 4 - Repeat status change
+         * 5 - Toggle repeat
+         * 6 - Seek to position
+        */
+
+        protected override void OnMessage(MessageEventArgs e) {
+            //I'm not going to simply get the playback data on every message received
+            //That would be a lot of unneeded requests
+            //and I'm already getting ratelimited
+            int commandCode = int.Parse(e.Data[0].ToString());
+            string commandData = e.Data.Remove(0, 1);
+            Console.WriteLine($"Command {commandCode} received, data: {commandData}");
+            try {
+                switch (commandCode) {
+                    case 0:
+                        var playback = Program.spotify.Player.GetCurrentPlayback().Result;
+                        if (playback == null) {
+                            Console.WriteLine("No playback detected!");
+                            break;
+                        }
+                        if (playback.IsPlaying) {
+                            Program.spotify.Player.PausePlayback().Wait();
+                        }
+                        else {
+                            Program.spotify.Player.ResumePlayback().Wait();
+                        }
+                        break;
+                    case 1:
+                        Program.spotify.Player.SkipPrevious().Wait();
+                        break;
+                    case 2:
+                        Program.spotify.Player.SkipNext().Wait();
+                        break;
+                    case 3:
+                        Program.SendInfo();
+                        break;
+                    case 4:
+                        Program.repeatNum = int.Parse(commandData);
+                        PlayerSetRepeatRequest repeatRequest = new((PlayerSetRepeatRequest.State)Program.repeatNum);
+                        Program.spotify.Player.SetRepeat(repeatRequest).Wait();
+                        break;
+                    case 5:
+                        var playback2 = Program.spotify.Player.GetCurrentPlayback().Result;
+                        if (playback2 == null) {
+                            Console.WriteLine("No playback detected!");
+                            break;
+                        }
+                        bool doShuffle;
+                        if (playback2.ShuffleState) {
+                            doShuffle = false;
+                        }
+                        else {
+                            doShuffle = true;
+                        }
+                        PlayerShuffleRequest shuffleRequest = new PlayerShuffleRequest(doShuffle);
+                        Program.spotify.Player.SetShuffle(shuffleRequest).Wait();
+                        break;
+                    case 6:
+                        PlayerSeekToRequest seekRequest = new(int.Parse(commandData));
+                        Program.spotify.Player.SeekTo(seekRequest).Wait();
+                        break;
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Exception caught: {ex}");
+                Program.SendInfo();
+            }
+        }
+
+        protected override void OnOpen() {
+            Console.WriteLine("New connection opened, list of sessions:");
+            Console.WriteLine(string.Join("\n", Sessions.ActiveIDs.ToArray()));
+        }
+
+        protected override void OnClose(CloseEventArgs e) {
+            Console.WriteLine($"A connection was closed! Reason: {e.Reason}, Code: {e.Code}");
+        }
+    }
+}
---
+++ NeosSpotifyStatus/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NeosSpotifyStatus;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using WebSocketSharp.Server;
+using IniParser;
+using IniParser.Model;
+
+class Program
+{
+    /*
+     * List of the prefixes/commands sent to Neos via WebSockets:
+     * 0 - clear everything
+     * 1 - Title
+     * 2 - Artist(s)
+     * 3 - Cover URL
+     * 4 - Album name
+     * 5 - Current song position (in ms)
+     * 6 - Total song duration (in ms)
+     * 7 - Is playing
+     * 8 - Repeat state
+     * 9 - Shuffle state
+     */
+
+    public static SpotifyClient spotify;
+    private static EmbedIOAuthServer _server;
+    public static WebSocketServer wsServer = new WebSocketServer(System.Net.IPAddress.Loopback, 1011, false);
+    public static int repeatNum;
+    static FileIniDataParser parser = new FileIniDataParser();
+    static IniData data = parser.ReadFile("config.ini");
+
+    public static async void SendInfo() {
+        var currentPlayback = await spotify.Player.GetCurrentPlayback();
+        if (currentPlayback == null) {
+            Console.WriteLine("No playback detected!");
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast("0");
+            return;
+        }
+        if (currentPlayback.Item is FullTrack track) {
+            List<string> artists = new List<string>();
+            foreach (SimpleArtist artist in track.Artists.ToArray()) {
+                artists.Add(artist.Name);
+            }
+
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, track.DurationMs);
+            TimeSpan position = new TimeSpan(0, 0, 0, 0, currentPlayback.ProgressMs);
+            var covers = track.Album.Images.ToArray();
+
+            //Send messages to Neos
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"1{track.Name}");
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"2{string.Join<string>(", ", artists)}");
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"3{covers[0].Url}");
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"4{track.Album.Name}");
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"5{currentPlayback.ProgressMs}");
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"6{track.DurationMs}");
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"7{currentPlayback.IsPlaying}");
+            switch (currentPlayback.RepeatState) {
+                case "off":
+                    repeatNum = 2;
+                    break;
+                case "context":
+                    repeatNum = 1;
+                    break;
+                case "track":
+                    repeatNum = 0;
+                    break;
+            }
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"8{repeatNum}");
+            wsServer.WebSocketServices["/neos-spotify-bridge"].Sessions.Broadcast($"9{currentPlayback.ShuffleState}");
+        }
+        if (currentPlayback.Item is FullEpisode episode) {
+            //Podcasts not supported (yet)
+        }
+    }
+
+    private static async Task OnAuthorizationCodeReceived(object sender, AuthorizationCodeResponse response) {
+        await _server.Stop();
+
+        var config = SpotifyClientConfig.CreateDefault();
+        var tokenResponse = await new OAuthClient(config).RequestToken(
+          new AuthorizationCodeTokenRequest(
+            data["Spotify"]["ClientID"], data["Spotify"]["ClientSecret"], response.Code, new Uri("http://localhost:5000/callback")
+          )
+        );
+
+        spotify = new SpotifyClient(tokenResponse.AccessToken);
+
+        var user = await spotify.UserProfile.Current();
+        Console.WriteLine($"Logged in as {user.DisplayName}");
+
+        while (true) {
+            System.Threading.Thread.Sleep(12500);
+            SendInfo();
+        }
+    }
+
+    private static async Task OnErrorReceived(object sender, string error, string state) {
+        Console.WriteLine($"Aborting authorization, error received: {error}");
+        await _server.Stop();
+    }
+
+    static async Task Main() {
+        Console.Title = "Neos Spotify Status";
+
+        
+
+        // Make sure "http://localhost:5000/callback" is in your spotify application as redirect uri!
+        _server = new EmbedIOAuthServer(new Uri("http://localhost:5000/callback"), 5000);
+        await _server.Start();
+
+        _server.AuthorizationCodeReceived += OnAuthorizationCodeReceived;
+        _server.ErrorReceived += OnErrorReceived;
+
+        var request = new LoginRequest(_server.BaseUri, data["Spotify"]["ClientID"], LoginRequest.ResponseType.Code) {
+            Scope = new List<string> { Scopes.UserReadCurrentlyPlaying, Scopes.UserModifyPlaybackState, Scopes.UserReadPlaybackState, Scopes.UserReadPlaybackPosition, Scopes.UserLibraryRead, Scopes.UserReadPrivate }
+        };
+        BrowserUtil.Open(request.ToUri());
+        
+        Console.WriteLine("WebSocket Server running at: " + wsServer.Address + ":" + wsServer.Port);
+        wsServer.AddWebSocketService<MainBehavior>("/neos-spotify-bridge");
+        wsServer.Start();
+
+        Console.ReadLine();
+    }
+}
